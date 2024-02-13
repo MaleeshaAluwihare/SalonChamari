8934e8170b24b72303b09673046bfa28e6fca2a1
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2024-02-13 10:57:56 +0530

    Created Basic structure of Backend

---
+++ BACKEND/Server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors  = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+const PORT = process.env.PORT || 8070;  /*when we import the web application to server we need to give a port number of a server 
+to open our application. process.env.Port allow to use available port at the time we import 
+web app to server or if we know the available ports on server we can directly give port number*/
+
+app.use(cors());
+app.use(bodyParser.json());  //json format mean the key value pairs
+
+const URL = process.env.MONGODB_URL  //''; paste the exact url aswell here.
