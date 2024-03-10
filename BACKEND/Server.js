7a88e6dec75d28f5c80d87cd37570dab60139c67
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2024-03-10 14:45:41 +0530

    import a router for search in server.js

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const cors  = require("cors");
 const dotenv = require("dotenv");
 const app = express();
+const serviceRouter = require("./Routes/servicesRoute.js");
+const searchRouter = require("./Routes/searchService.js");
 
 require("dotenv").config();
 
@@ -33,7 +35,6 @@ app.listen(PORT, () => {
 })
 
 
-const serviceRouter = require("./Routes/servicesRoute.js");  //import the service route
-
 //when data passing to frontend to backend its calling a url (http://localhost:8070/service) then the services.js in routes will be loaded.
 app.use("/service",serviceRouter)
+app.use("/service",searchRouter)
