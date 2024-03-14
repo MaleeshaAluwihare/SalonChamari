29179887ed285cd244e97dc1be70248212d9963d
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-03-15 00:29:18 +0530

    Updated FaqModel.js

---
+++ BACKEND/Models/Dasun/FaqModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const faqSchema = new Schema({
+
+    question : {
+        type : String,
+        require : true
+    },
+
+    answer : {
+        type : String,
+        require : true
+    }
+
+})
+
+const Faq = mongoose.model("faq", faqSchema)
+
+module.exports = Faq;
