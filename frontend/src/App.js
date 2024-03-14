1f9d63e7b5055b122b450cdf9f1e83065a5fd112
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2024-03-14 15:16:09 +0530

    tested the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import CounterClass from './components/ClassBaseApproch';
+import CounterFunction from './components/CounterFunction';
 
 function App() {
   return (
     <div className="App">
         <h1>Hello</h1>
+        <CounterFunction/>
+        <hr></hr>
+        <CounterClass/>
     </div>
   );
 }
