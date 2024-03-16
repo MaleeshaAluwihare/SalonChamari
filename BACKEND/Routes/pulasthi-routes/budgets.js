ab69001a60b33c9db5e9d8209dc0153b8705ca82
Pulasthi Buddika <151649555+pulasthiBuddikaGit@users.noreply.github.com>
2024-03-16 13:24:03 +0530

    create getBudgetById route

--- BACKEND/Routes/pulasthi-routes/budgets.js
+++ BACKEND/Routes/pulasthi-routes/budgets.js
@@ -34,5 +34,27 @@ router.route("/get-budgets").get((req, res) => {
     })
 })
 
+//get specific booking
+router.route("/getBudgetById/:budgetId").get(async (req, res) => {
+    let budgetId = req.params.budgetId;
+
+    try {
+        // Find the booking by custom ID
+        //save the finding budget to budget constant
+        const budget = await BudgetTable.findOne({ budgetId: budgetId });
+
+        // Check if booking is found
+        if (!budget) {
+            return res.status(404).send({ status: "Budget_Not_Found" });
+        }
+
+        // Send the booking data to the frontend
+        res.status(200).send({ status: "Budget fetched", budget: budget });
+    } catch (error) {
+        // Handle errors
+        console.error("Error fetching budget:", error);
+        res.status(500).send({ status: "Internal_Server_Error" });
+    }
+});
 
 module.exports = router;
