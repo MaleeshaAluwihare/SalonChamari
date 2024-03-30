07bb129957f4c2d30f1fd8465d84af3c855daa92
Nisalka Jayasinghe <jayasinghenisalka@gmail.com>
2024-03-30 19:13:06 +0530

    Update client_m.js

--- BACKEND/Models/nisalka_client/client_m.js
+++ BACKEND/Models/nisalka_client/client_m.js
@@ -16,25 +16,33 @@ const CaccountSchema = new mongoose.Schema({
     minlength: 3,
     maxlength: 50,
   },
-  email: {
+  Email: {
     type: String,
     required: true,
     minlength: 5,
     maxlength: 255,
   },
-  password: {
+  Password: {
     type: String,
     required: true,
     minlength: 8,
     maxlength: 1024,
   },
-  phone: {
-    type: String,
+  Phone: {
+    type: Number,
+    required: true,
     minlength: 10,
     maxlength: 15,
   },
-  address: {
+  Age: {
+    type: Number,
+    required: true,
+    minlength: 2,
+    maxlength: 3,
+  },
+  Gender: {
     type: String,
+    required: true,
     minlength: 10,
     maxlength: 255,
   },
