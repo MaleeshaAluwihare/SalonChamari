ff3d00b715bc2afcf6053eb6971af24f71fd875c
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-04-03 13:26:00 +0530

    Update faqsRoute.js

--- BACKEND/Routes/Dasun/faqsRoute.js
+++ BACKEND/Routes/Dasun/faqsRoute.js
@@ -33,4 +33,102 @@ router.route("/add").post((req,res) => {
 })
 
 
+
+//DISPLAY
+//http: //localhost:8070/Faqs/display
+router.route("/display").get((req,res) => {
+
+    Faqs.find().then((Faqs) => {
+
+        res.json(Faqs);
+
+    }).catch((err) => {
+
+        console.log(err.message);
+        res.status(500).send({status: "Error with get faqs", error: err.message});
+
+    })
+
+})
+
+
+
+
+//GetOne
+//http: //localhost:8070/Faqs/get/
+router.route("/get/:id").get(async(req,res) => {
+
+    let faqId = req.params.id;
+
+    const faq = await Faqs.findById(faqId).then((Faqs) => {
+
+        res.status(200).send({status: "Faq fetched", Faqs});
+
+    }).catch((error) => {
+
+        console.log(error);
+        res.status(500).send({status: "Error with get faq", error: error.message});
+
+    })
+
+})
+
+
+
+
+
+//UPDATE
+//http: //localhost:8070/Faqs/update/
+router.route("/update/:id").put(async(req,res) => {
+
+    let faqId = req.params.id;
+    
+    const{question, answer} = req.body;
+
+    const updateFaq = {
+
+        question,
+        answer
+
+    }
+
+    const update = await Faqs.findByIdAndUpdate(faqId, updateFaq).then(() => {
+
+        res.status(200).send({status: "Faq updated"});
+
+    }).catch((error) => {
+
+        console.log(error);
+        res.status(500).send({status: "Error with updating data"});
+
+    })
+
+})
+
+
+
+
+
+//DELETE
+//http: //localhost:8070/Faqs/delete/
+router.route("/delete/:id").delete(async(req,res) => {
+
+    let faqId = req.params.id;
+
+    await Faqs.findByIdAndDelete(faqId).then(() => {
+
+        res.status(200).send({status: "Faq deleted"});
+
+    }).catch((error) => {
+
+        console.log(error);
+        res.status(500).send({status: "Error with deleting faq", error: error.message});
+
+    })
+
+})
+
+
+
+
 module.exports = router;
