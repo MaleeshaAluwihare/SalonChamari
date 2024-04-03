1c0fda6ce820e8607e535ff3c18893b30d7daca6
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-04-03 18:12:04 +0530

    Create and Update FeedbackRoute.js

---
+++ BACKEND/Routes/Dasun/FeedbackRoute.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+let Feedbacks = require("../../Models/Dasun/FeedbackModel");
+
+
+
+
+
+//ADD
+//http: //localhost:8070/Feedbacks/add
+router.route("/add").post(async (req,res) => {
+
+    const{feedbackId, bookingId, category, content, sendDate} = req.body;
+
+    try{
+
+        const newFeedback = new Feedbacks({feedbackId, bookingId, category, content, sendDate});
+        await newFeedback.save();
+
+        res.json("Feedback Added");
+
+    } catch(err) {
+
+        console.error(err.message);
+        res.status(500).send("Error with adding Feedback");
+
+    }
+
+
+});
