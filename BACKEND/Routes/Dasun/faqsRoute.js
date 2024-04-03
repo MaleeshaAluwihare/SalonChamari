26ec599c99307f119ba2a5008e6178ca60035e58
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-04-03 10:32:39 +0530

    Create faqsRoute.js

---
+++ BACKEND/Routes/Dasun/faqsRoute.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+let Faqs = require("../../Models/Dasun/FaqsModel");
+
+
+
+
+
+//ADD
+//http: localhost:8070/Faqs/add
+router.route("/add").post((req,res) => {
+
+    const question = req.body.Question;
+    const answer = req.body.Answer;
+
+    const newFaq = new Faqs({
+
+        question,
+        answer
+
+    })
+
+    newFaq.save().then(() => {
+
+        res.json("New Faq Added")
+
+    }).catch((err) => {
+
+        res.status(500).json({error : "Failed to add faq"})
+
+    })
+
+})
+
+
+module.exports = router;
