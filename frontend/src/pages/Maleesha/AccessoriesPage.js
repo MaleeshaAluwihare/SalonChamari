00d73198324191717c5c3bd97339a6f6fcd6307d
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2024-04-06 12:04:57 +0530

    image upload form edited

---
+++ frontend/src/pages/Maleesha/AccessoriesPage.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+// import axios from 'axios';
+
+export default function AccessoriesImagePage(){
+
+
+    return(
+        <div className='mainContainer'>
+            <div className='brideContainer'>
+                <h2>Bride Costumes</h2>
+                <hr></hr>
+            </div>
+            <div className='groomContainer'>
+                <h2>Groom Costumes</h2>
+                <hr></hr>
+            </div>
+        </div>
+    );
+}
