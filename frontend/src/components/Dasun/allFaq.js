602600b33d5b991b3a268a48357aa9c72f141e59
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-04-09 18:30:41 +0530

    Update allFaq.js

--- frontend/src/components/Dasun/allFaq.js
+++ frontend/src/components/Dasun/allFaq.js
@@ -2,14 +2,57 @@ import React, {useState, useEffect} from "react";
 
 export default function AllFaqs() {
 
+    const [faqs, setFaqs] = useState([]);
+
+    useEffect(() => {
+
+        const allFaqs = async () => {
+
+            try{
+
+                const response = await fetch('http://localhost:8070/Faqs/display');
+                const data = await response.json();
+
+                setFaqs(data);
+
+            } catch(error) {
+
+                console.error('Error with fetching faqs : ', error);
+
+            }
+
+        }
+        allFaqs();
+
+    }, [])
+
     return(
 
         <div>
 
             <h1>All Faqs</h1>
 
+            <table>
+                <thead>
+                    <tr>
+                        <th>Question</th>
+                        <th>Answer</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {faqs.map(faqs => (
+                        <tr key={faqs._id}>
+                            <td>{faqs.question}</td>
+                            <td>{faqs.answer}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
         </div>
 
     )
 
 }
+
