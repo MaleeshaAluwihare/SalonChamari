e79a2db6e86e776552bb4714de2ca3a918ebbd76
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-04-10 01:31:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from './components/Dasun/Header';
 import AddFaq from './components/Dasun/addFaq';
 import AllFaqs from './components/Dasun/allFaq';
+import UpdateFaq from './components/Dasun/updateFaq';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
 
             <Route path='/faq/all' exact Component={AllFaqs} /> 
             <Route path="/faq/add" exact Component={AddFaq} /> 
+            <Route path="/faq/update/${faqs._id}" exact Component={UpdateFaq} />
 
           </Routes>
 
