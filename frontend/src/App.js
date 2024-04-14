064b291646e28fb5ea8294de24cd2a726fcced2e
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-04-14 11:28:45 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import AddFaq from './components/Dasun/addFaq';
 import AllFaqs from './components/Dasun/allFaq';
 import UpdateFaq from './components/Dasun/updateFaq';
 import Messages from './components/Dasun/Messages';
+import Feedbacks from './components/Dasun/AllFeedbacks';
+import ReplyMessage from './components/Dasun/ReplyMessage';
 
 
 function App() {
@@ -22,6 +24,8 @@ function App() {
             <Route path="/faq/add" exact Component={AddFaq} /> 
             <Route path="/faq/update/${faqs._id}" exact Component={UpdateFaq} />
             <Route path="/CustomerMessages/all" exact Component={Messages} />
+            <Route path="/feedback/all" exact Component={Feedbacks} />
+            <Route path="/ReplyMessage/add" exact Component={ReplyMessage} />
 
           </Routes>
 
