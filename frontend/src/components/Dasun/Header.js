e87ffcf0b69dca30fbba1fc5911983c3c9b92cf0
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-04-16 00:41:23 +0530

    Update Header.js

--- frontend/src/components/Dasun/Header.js
+++ frontend/src/components/Dasun/Header.js
@@ -14,8 +14,8 @@ function Header() {
             <ul>
             <li><Link to="/faq/all">FAQs</Link></li>
             <li><Link to="/faq/add">Add FAQ</Link></li>
-            <li><Link to="#">Blogs</Link></li>
-            <li><Link to="#">Add Blog</Link></li>
+            <li><Link to="/blog/all">Blogs</Link></li>
+            <li><Link to="/blog/add">Add Blog</Link></li>
             <li><Link to="/CustomerMessages/all">Messages</Link></li>
             <li><Link to="/feedback/all">Feedbacks</Link></li>
             </ul>
