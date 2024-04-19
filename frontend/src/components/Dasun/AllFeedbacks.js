f98bec72f9e64ad56a0bdfbb9069c755b4e19ff7
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-04-19 23:10:05 +0530

    Update AllFeedbacks.js

--- frontend/src/components/Dasun/AllFeedbacks.js
+++ frontend/src/components/Dasun/AllFeedbacks.js
@@ -32,7 +32,15 @@ export default function Feedbacks() {
     const formatDate = (dateString) => {
 
         const date = new Date(dateString);
-        return date.toLocaleDateString();
+        
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+
+        const formattedDay = day < 10 ? `0${day}`: day;
+        const formattedMonth = month < 10 ? `0${month}`: month;
+
+        return `${formattedDay}/${formattedMonth}/${year}`;
 
     };
 
