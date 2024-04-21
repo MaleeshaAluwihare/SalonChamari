f0fca6a805663713b528c7aed8424e9f9e3b2824
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-04-22 00:33:15 +0530

    Update allFaq.js

--- frontend/src/components/Dasun/allFaq.js
+++ frontend/src/components/Dasun/allFaq.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import '../../CSS/allFaq.css';
+import '../../CSS/displayFaq.css';
 import { Link } from "react-router-dom";
 
 export default function AllFaqs() {
