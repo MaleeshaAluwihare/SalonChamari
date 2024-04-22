2eb87d9981e3ffc31ba7772391d13d82700e8424
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-04-22 12:30:50 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import './CSS/App.css';
+import './CSS/Dasun/App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from './components/Dasun/Header';
 import AddFaq from './components/Dasun/addFaq';
