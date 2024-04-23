33396bec99c990523b37b510cdba2206a4dfcd60
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-04-23 22:49:13 +0530

    Update ClientHome.js

--- frontend/src/pages/ClientHome.js
+++ frontend/src/pages/ClientHome.js
@@ -16,6 +16,7 @@ function ClientHome() {
             <li><Link to="/feedbackCustomer">Feedback</Link></li>
             <li><Link to="/blogCustomer">Blogs</Link></li>
             <li><Link to="/messageCustomer">Messages</Link></li>
+            <li><Link to="#">Message Replies</Link></li>
             </ul>
         </nav>
         </div>
