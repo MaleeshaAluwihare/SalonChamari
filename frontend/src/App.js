016efdb9a114817fb1d0dd5356daba833c35502b
Maleesha Aluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2024-05-07 19:18:20 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter as Router and Routes
 import './App.css';
+import { Navigate } from "react-router-dom";
+
 
 //nisalka
 import Profile from "../src/components/nisalka/Profile";
 import Signup from "../src/components/nisalka/Signup";
 import Login from "../src/components/nisalka/Login";
-import { Navigate } from "react-router-dom";
 import CMdashboard from "./components/nisalka/CMdashboard";
-import Forgot from "./components/nisalka/forgotpassword"
+import Forgot from "./components/nisalka/forgotpassword";
 
 
 //Chavidu
@@ -87,7 +88,7 @@ import EventWrapper from './components/Pulasthi/Wrapper/EventWrapper';
 
 
 function App() {
-  
+  const user = localStorage.getItem("token");
   return (
     <div className="App">
 
@@ -148,6 +149,18 @@ function App() {
               <Route path="/salary" element={<EmployeeWrapper/>} />
               <Route path="/inventory" element={<InventoryWrapper />} />
               <Route path="/packages" element={<EventWrapper/>} />
+
+              {user && <Route path="/profile/:email" exact element={<Profile />} />}
+			        <Route path="/signup" exact element={<Signup />} />
+			        <Route path="/login" exact element={<Login />} />
+              <Route path="/login" element={<Navigate replace to="/login" />} />
+
+              {/*  admin login */}
+              <Route path="/cmdash" element={<CMdashboard/>}/>
+              <Route path="/Uactivity" element={<CMdashboard/>}/>
+              <Route path="/Memails" element={<CMdashboard/>}/>
+              <Route path="/forgot" element={<Forgot/>}/>
+
 
           </Routes>
 
